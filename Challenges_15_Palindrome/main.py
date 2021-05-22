3a19c7751ca4e0427ac71c6b5cd582bcc3413750
jonas <jonasbullwinkel8@gmail.com>
2021-05-22 14:41:22 +0200

    Update main.py

--- Challenges_15_Palindrome/main.py
+++ Challenges_15_Palindrome/main.py
@@ -1,12 +1,33 @@
 import requests
 import json
 
-URLchallenge = "https://cc.the-morpheus.de/challenges/14/"
-URLsolution = "https://cc.the-morpheus.de/solutions/14/"
+URLchallenge = "https://cc.the-morpheus.de/challenges/15/"
+URLsolution = "https://cc.the-morpheus.de/solutions/15/"
+
+resp = requests.get(URLchallenge).json()['word']
 
 def solve(resp):
-    print(resp)
-    return 1
+    data = ""
+    resp = resp.strip("/n")
+    for i in resp:
+        if i == " " or i == "'" or i == "," or i == "." or i == "!" or i == "?" or i == "-" or i == '"' or i == ":" or i == ";" or i == "/":
+            continue
+        data = data + i
 
-resp = requests.get(URLchallenge).json()['list']
-print(requests.post(URLsolution, data=json.dumps({"token": solve(resp)})).text)
+    data = data[0:len(data)-1]
+    i = len(data)-1
+    redata = ""
+    while i >= 0:
+        redata = redata + data[i]
+        i = i -1
+    
+    redata = redata.lower()
+    data = data.lower()
+
+    if redata == data:
+        return True
+    return False
+
+for i in range(10):
+    resp = requests.get(URLchallenge).json()['word']
+    print(requests.post(URLsolution, data=json.dumps({"token": solve(resp)})).text)
