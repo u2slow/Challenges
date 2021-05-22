ad2addd1e36a69e63e2bfad62d2f22b8293d3449
jonas <jonasbullwinkel8@gmail.com>
2021-05-22 15:06:05 +0200

    Update main.py

--- Challenges_16_Längste_Zahlenfolge/main.py
+++ Challenges_16_Längste_Zahlenfolge/main.py
@@ -7,6 +7,19 @@ URLsolution = "https://cc.the-morpheus.de/solutions/16/"
 resp = requests.get(URLchallenge).json()['list']
 
 def solve(resp):
-    print(resp)
-
-print(requests.post(URLsolution, data=json.dumps({"token": solve(resp)})).text)
+    resp.sort()
+    sol = 1
+    val = 1
+    for i in range(len(resp)-1):
+        if resp[i] == resp[i+1]-1:
+            val = val +1 
+            if val > sol:
+                sol = val
+        elif resp[i] == resp[i+1]:
+            pass
+        else:
+            val = 1
+    return sol
+for i in range(50):
+    resp = requests.get(URLchallenge).json()['list']
+    print(requests.post(URLsolution, data=json.dumps({"token": solve(resp)})).text)
