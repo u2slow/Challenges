a869a08405556da8b48e1887000c0da750c91c29
u2slow <58704917+u2slow@users.noreply.github.com>
2021-06-09 18:46:02 +0200

    .

--- Pathfinder/main.py
+++ Pathfinder/main.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 class environment:
     def __init__(self, size, start, destination, blocketarea):
         self.blocketarea = blocketarea
@@ -7,24 +9,52 @@ class environment:
         self.createEnvironment()
     
     def show(self):
-            for i in self.background:
-                print(i)
-            print("")
+        plist = []
+        for i in range(len(self.background)):
+            plist.append([])
+            for n in range(len(self.background[i])):
+                plist[i].append(self.background[i][n].value)
+            print(plist[i])
+        print("")
 
     def createEnvironment(self):
         self.background = []
         for i in range(self.size):
             self.background.append([])
-            self.background[i] = [0]*self.size
+            for n in range(self.size):
+                self.background.append(point([i,n],status.background,[]))
         for i in self.blocketarea:
-            self.background[i[0]][i[1]] = 1
-        self.background[self.start[0]][self.start[1]] = 3
-        self.background[self.destination[0]][self.destination[1]] = 4
+            print(self.background[i[0]][i[1]].getStatus)
+            self.background[i[0]][i[1]].status = status.blocked
+        self.background[self.start[0]][self.start[1]] = status.start
+        self.background[self.destination[0]][self.destination[1]] = status.destination
+        self.search = [point(self.start, status.start)]
 
-    def findPath():
-        
-        
+    def findPath(self):
+        for i in self.search:
+            if self.background[i.location[0]][i.location[1]] == status.destination:
+                pass
 
+class status(Enum):
+    background = 0
+    blocked = 1
+    start = 3
+    destination = 4
+    search = 5
+    lastsearch = 6
+    searched = 7
+    
+class point:
+    def __init__(self,location,status, parent):
+        self.location = location
+        self.status = status
+        self.parent = parent
+    def getStatus(self):
+        return self.status
+    def getLocation(self):
+        return self.location
+    def getParent(self):
+        return self.parent
 if __name__ == '__main__':
     env = environment(8,[1,1],[6,6], [[2,3],[3,3],[4,3]])
     env.show()
