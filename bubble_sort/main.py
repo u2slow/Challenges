43d2e12e33e5daf752a16c0b91886d09ce4bf71f
jonas <jonasbullwinkel8@gmail.com>
2022-11-30 18:15:37 +0100

    .

---
+++ bubble_sort/main.py
@@ -0,0 +1,23 @@
+import random
+
+unsort_list = []
+
+for i in range(10):
+    unsort_list.append(random.randint(0,100))
+
+print(unsort_list)
+
+for n in range(len(unsort_list)):
+
+    #print(unsort_list)
+
+    for i in range(len(unsort_list)):
+        if i + 1 + n >= len(unsort_list):
+            break
+        
+        if unsort_list[i] > unsort_list[i+1]:
+            mem = unsort_list[i]
+            unsort_list[i] = unsort_list[i+1]
+            unsort_list[i+1] = mem
+
+print(unsort_list)
